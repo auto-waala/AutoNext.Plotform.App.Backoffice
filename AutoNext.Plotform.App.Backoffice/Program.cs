6534a1cdbb1fce7e1c8d83c7674fb9d829b9f7b8
Durga Prasad <bhagatprasad8@yahoo.com>
2026-04-15 21:21:53 +0530

    Add project files.

---
+++ AutoNext.Plotform.App.Backoffice/Program.cs
@@ -0,0 +1,32 @@
+using AutoNext.Plotform.App.Backoffice.Client.Pages;
+using AutoNext.Plotform.App.Backoffice.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(AutoNext.Plotform.App.Backoffice.Client._Imports).Assembly);
+
+app.Run();
